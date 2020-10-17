a8be332783a5b47a7e7e2ff6d61b3f9b88a94ac3
MALLASSI Olivier <Olivier.MALLASSI@gmail.com>
2020-10-17 19:16:36 +0200

    work on #13

--- src/component/domains/ListDomainsComponent.jsx
+++ src/component/domains/ListDomainsComponent.jsx
@@ -33,17 +33,16 @@ class ListDomainsComponent extends Component {
         //
         ApiService.getDomainsMetrics().then((res) => {
             var response = res.data;
-            var data_table = [['Name', 'Parent', 'Resources Number (num)']]
+            var data_table = [['Name', 'Parent', 'Resources Number (num)'], ['Global', null, 0]]
 
             var max = 2;
             for (var index in response) {
-                console.log(data_table.length);
-                console.log([response[index].name, response[index].parent, response[index].value]);
-
+                //column 0 is the ID and must be unique
                 data_table[data_table.length] = [response[index].name, response[index].parent, response[index].value];
             }
 
             console.log(data_table);
+            console.log(data_table.length);
 
             //we should map it to
             //[
@@ -72,8 +71,45 @@ class ListDomainsComponent extends Component {
                             chartType="TreeMap"
                             loader={<div>Loading Chart</div>}
                             data={this.state.stats}
+                            // data={[
+                            //     [
+                            //         'Location',
+                            //         'Parent',
+                            //         'Market trade volume (size)',
+                            //         'Market increase/decrease (color)',
+                            //     ],
+                            //     ['Global', null, 0, 0],
+                            //     ['America', 'Global', 0, 0],
+                            //     ['Europe', 'Global', 0, 0],
+                            //     ['Asia', 'Global', 0, 0],
+                            //     ['Australia', 'Global', 0, 0],
+                            //     ['Africa', 'Global', 0, 0],
+                            //     ['Brazil', 'America', 11, 10],
+                            //     ['USA', 'America', 52, 31],
+                            //     ['Mexico', 'America', 24, 12],
+                            //     ['Canada', 'America', 16, -23],
+                            //     ['France', 'Europe', 42, -11],
+                            //     ['Germany', 'Europe', 31, -2],
+                            //     ['Sweden', 'Europe', 22, -13],
+                            //     ['Italy', 'Europe', 17, 4],
+                            //     ['UK', 'Europe', 21, -5],
+                            //     ['China', 'Asia', 36, 4],
+                            //     ['Japan', 'Asia', 20, -12],
+                            //     ['India', 'Asia', 40, 63],
+                            //     ['Laos', 'Asia', 4, 34],
+                            //     ['Mongolia', 'Asia', 1, -5],
+                            //     ['Iran', 'Asia', 18, 13],
+                            //     ['Pakistan', 'Asia', 11, -52],
+                            //     ['Egypt', 'Africa', 21, 0],
+                            //     ['S. Africa', 'Africa', 30, 43],
+                            //     ['Sudan', 'Africa', 12, 2],
+                            //     ['Congo', 'Africa', 10, 12],
+                            //     ['Zaire', 'Africa', 8, 10],
+                            // ]}
                             options={{
                                 highlightOnMouseOver: true,
+                                maxDepth: 1,
+                                maxPostDepth: 2,
                                 minHighlightColor: '#8c6bb1',
                                 midHighlightColor: '#9ebcda',
                                 maxHighlightColor: '#edf8fb',
