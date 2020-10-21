b75df1671c1202e42ade8578db16cfef266aefb7
MALLASSI Olivier <Olivier.MALLASSI@gmail.com>
2020-10-21 08:23:41 +0200

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -62,8 +62,8 @@ export default function ListRouter() {
         <Paper elevation={0}>
           <List aria-label="main mailbox folders">
             <ListItemLink to="/" primary="Dashboard" icon={<DashboardIcon />} />
-            <ListItemLink to="/apis" primary="Apis" icon={<ListAltIcon />} />
             <ListItemLink to="/domains" primary="Domains" icon={<FilterTiltShiftIcon />} />
+            <ListItemLink to="/apis" primary="Apis" icon={<ListAltIcon />} />
             <ListItemLink to="/envs" primary="Environments" icon={<ComputerIcon />} />
             <li>
               <a className="MuiButtonBase-root MuiListItem-root MuiListItem-gutters MuiListItem-button" tabIndex="0" role="button" aria-disabled="false"
