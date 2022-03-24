9c87edb8923fdc647e5e89e4f3080924aee8ad9d
omallassi <olivier.mallassi@gmail.com>
2022-03-24 15:14:37 +0100

    Update entrypoint.sh

--- docker-config/entrypoint.sh
+++ docker-config/entrypoint.sh
@@ -23,4 +23,4 @@ cd /apis-catalog-web/ && npm run build
 # Start the server
 echo -e "\033[1;32m*** Starting apis-catalog Server ***\033[0m"
 export API_SERVER_CONFIG_FILE=/apis-catalog/config/local.toml
-RUST_LOG="info,apis_catalog_server=debug" /apis-catalog/target/debug/apis_catalog_server
+RUST_LOG="info,apis_catalog_server=debug" /apis-catalog/apis_catalog_server
